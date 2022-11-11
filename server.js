5aebb418d542c2b26890f5f5953870dbf9e7d6c8
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-11 15:30:16 +0100

    Add socket endpoints

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+  res.status(404).send({ message: "Not found..." });
+});
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Client connected with ID:", socket.id);
+  socket.broadcast.emit("updateData", tasks);
+
+  socket.on("addTask", (task) => {
+    console.log("Get new task" + task);
+    tasks.push(task);
+    socket.broadcast.emit("addTask", task);
+  });
+
+  socket.on("removeTask", (removeTask) => {
+    tasks.splice(tasks.indexOf(removeTask), 1);
+    socket.broadcast.emit("removeTask", removeTask);
+  });
 });
